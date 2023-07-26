2b8704114dce50f64748bb202ddbe3043e70e300
Milly <83847194+Milly66k@users.noreply.github.com>
2023-07-26 19:25:09 -0300

    Adicionei o carrinho e alterei os cards

--- script.js
+++ script.js
@@ -20,4 +20,52 @@ function showSection(sectionId) {
   
   // Mostrar a seção inicial (home) ao carregar a página
   showSection('hamburgers');
+
+
+  //Carrinho de compras
+
   
+
+  // Definir uma variável para armazenar os itens do carrinho
+let cartItems = [];
+
+// Função para adicionar um item ao carrinho
+function addToCart(itemName, itemPrice) {
+  cartItems.push({ name: itemName, price: itemPrice });
+  updateCart();
+}
+
+// Função para remover um item do carrinho
+function removeFromCart(index) {
+  cartItems.splice(index, 1);
+  updateCart();
+}
+
+// Função para atualizar o carrinho no HTML
+function updateCart() {
+  const cartElement = document.querySelector('.cart-items');
+  const cartTotalElement = document.getElementById('cart-total');
+  cartElement.innerHTML = '';
+  let total = 0;
+
+  cartItems.forEach((item, index) => {
+    const itemElement = document.createElement('div');
+    itemElement.innerHTML = `${item.name} - R$ ${item.price.toFixed(2)} <button onclick="removeFromCart(${index})">Remover</button>`;
+    cartElement.appendChild(itemElement);
+    total += item.price;
+  });
+
+  cartTotalElement.textContent = `R$ ${total.toFixed(2)}`;
+}
+
+// Função para limpar o carrinho
+function clearCart() {
+  cartItems = [];
+  updateCart();
+}
+
+// Evento para limpar o carrinho ao clicar no botão "Limpar Carrinho"
+document.getElementById('clear-cart').addEventListener('click', clearCart);
+
+// Chame a função updateCart() para exibir o carrinho inicialmente vazio
+updateCart();
